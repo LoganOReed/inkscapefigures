5dbcc394f269de1717fce2462971cbde66f6f01e
LoganOReed <me@loganreed.org>
2025-09-28 10:44:18 -0400

    Fixed ink

--- inkscapefigures/main.py
+++ inkscapefigures/main.py
@@ -144,29 +144,19 @@ def maybe_recompile_figure(filepath):
     pdf_path = filepath.parent / (filepath.stem + '.pdf')
     name = filepath.stem
 
-    inkscape_version = subprocess.check_output(['inkscape', '--version'], universal_newlines=True)
-    log.debug(inkscape_version)
+    # inkscape_version = subprocess.check_output(['inkscape', '--version'], universal_newlines=True)
+    # log.debug(inkscape_version)
 
     # Convert
     # - 'Inkscape 0.92.4 (unknown)' to [0, 92, 4]
     # - 'Inkscape 1.1-dev (3a9df5bcce, 2020-03-18)' to [1, 1]
     # - 'Inkscape 1.0rc1' to [1, 0]
-    inkscape_version = re.findall(r'[0-9.]+', inkscape_version)[0]
-    inkscape_version_number = [int(part) for part in inkscape_version.split('.')]
+    # inkscape_version = re.findall(r'[0-9.]+', inkscape_version)[0]
+    # inkscape_version_number = [int(part) for part in inkscape_version.split('.')]
 
     # Right-pad the array with zeros (so [1, 1] becomes [1, 1, 0])
-    inkscape_version_number= inkscape_version_number + [0] * (3 - len(inkscape_version_number))
+    # inkscape_version_number= inkscape_version_number + [0] * (3 - len(inkscape_version_number))
 
-    # Tuple comparison is like version comparison
-    if inkscape_version_number < [1, 0, 0]:
-        command = [
-            'inkscape',
-            '--export-area-page',
-            '--export-dpi', '300',
-            '--export-pdf', pdf_path,
-            '--export-latex', filepath
-            ]
-    else:
         command = [
             'inkscape', filepath,
             '--export-area-page',
